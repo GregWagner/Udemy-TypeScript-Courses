15288a50706ba5ece7970d7ddbbd23c0a4d21e5d
GregWagner <theGreg101@gmail.com>
2025-11-15 07:36:14 -0600

    Section 1 redo

--- Typescript_The_Complete_Developer's_Guide/Section_01_Getting_Started_with_Typescript/fetchjson/index.ts
+++ Typescript_The_Complete_Developer's_Guide/Section_01_Getting_Started_with_Typescript/fetchjson/index.ts
@@ -1,22 +1,27 @@
 import axios from 'axios';
 
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
 interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
+    id: number;
+    title: string;
+    completed: boolean;
 }
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios.get(url).then(response => {
+    const todo = response.data as Todo;
 
-axios.get(url).then((response) => {
-  const todo = response.data as Todo;
-  const { id, title, completed } = todo;
-  logTodo(id, title, completed);
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+  logTodo(id, title, );
 });
 
-const logTodo = (id: number, title: string, completed: boolean) => {
-  console.log(`
-        The Todo with ID: ${id}
-        Has a title of: ${title}
-        Is it finished? ${completed}`);
-};
+const logTodo = (id: number, title: string, completed: boolean): void => {
+    console.log(`
+      The Todo with ID: ${id}
+      Has a title of: ${title}
+      Is it finished? ${completed}`
+    );
+}
